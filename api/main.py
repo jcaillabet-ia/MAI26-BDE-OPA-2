a6181e144c383c44ca2a4c501683c11b47f90e2a
Julien CAILLABET <jcaillabet@gmail.com>
2026-07-10 15:49:25 +0200

    Service et routes de gestion de la liste des cryptos

--- api/main.py
+++ api/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from routes import admin as admin_router
+from routes import coin as coin_router
 from routes import ingestion as ingestion_router
 
 app = FastAPI(title="FastAPI PostgreSQL Bridge")
@@ -15,4 +16,5 @@ app.add_middleware(
 )
 
 app.include_router(admin_router.router, prefix="/admin", tags=["admin"])
+app.include_router(coin_router.router, prefix="/coin", tags=["coin"])
 app.include_router(ingestion_router.router, prefix="/ingestion", tags=["ingestion"])
