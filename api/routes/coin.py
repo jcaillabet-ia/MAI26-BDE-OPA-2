5a299603278dddd73b3dcdbb838a66f8b39e4866
Julien CAILLABET <jcaillabet@gmail.com>
2026-07-17 12:21:33 +0200

    Préparation de Airflow

--- api/routes/coin.py
+++ api/routes/coin.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, status
+import httpx
 
 from services.coin import list_coins, enable_coin, disable_coin
 
@@ -10,11 +11,17 @@ def list():
 
 @router.patch("/{id}/enable", status_code=status.HTTP_204_NO_CONTENT)
 def enable(id: str):
-    coin = enable_coin(id)
-    return coin
+    enable_coin(id)
+    r = httpx.post("http://airflow-webserver:8080/api/v1/dags/cryptobot_coin_toggle/dagRuns", 
+            json={"conf":{
+                    "command": "enable", 
+                    "coin_id": id}
+                },
+            auth=("airflow", "airflow"))
+    print(r.status_code)
+    print(r.text)
 
 @router.patch("/{id}/disable", status_code=status.HTTP_204_NO_CONTENT)
-def enable(id: str):
-    coin = disable_coin(id)
-    return coin
+def disable(id: str):
+    disable_coin(id)
     
---
+++ dags/coin_toggle.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+from airflow.decorators import dag, task
+from airflow.utils.dates import days_ago
+
+@task
+def task1():
+    pass
+
+@dag(
+    dag_id='cryptobot_coin_toggle',
+    tags=['crypto_bot'],
+    schedule_interval = None,
+    start_date=days_ago(0),
+    catchup=False
+)
+def my_dag():
+    t1 = task1()
+
+my_dag = my_dag()
